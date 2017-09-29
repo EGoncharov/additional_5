d8433af574ef65d08dea9dcd4a99f2e89795e389
Eugene Goncharov <eugen.goncharov@gmail.com>
2017-09-29 17:31:00 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  var arrayOne = str.split('');
+  var arrayTwo = [];
+  
+  for (var i=0; i<arrayOne.length; i++) {
+    for (var x=0; x<bracketsConfig.length; x++) {
+      if (arrayOne[i] === bracketsConfig[x][0]) {
+        arrayTwo.push(arrayOne[i]);
+      }
+      if (arrayOne[i] === bracketsConfig[x][i] && arrayTwo[arrayTwo.length-1] === bracketsConfig[x][0]) {
+        arrayTwo.pop()
+      }
+    }
+  }
+  if (arrayTwo.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+      
 }
